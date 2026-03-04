9ba379c244e45620664c058b20799fb5dc363216
Lucca Rosa <luccaroosa12+github@gmail.com>
2026-03-04 19:50:59 -0300

    Aula sobre criação de tabelas
    Envio dos documentos pedidos pelo professor para mostrar o desempenho e avanço nas atividades de criação de tabelas no banco de dados.
    1. Imagem das tabelas importadas para o Azure SQL Server por meio do DBeaver.
    2. Imagem das tabelas importadas para o Azure SQL Server por meio do VS Code.
    3. Imagem do diagrama entidade relacionamento construído a partir do VS Code.
    4. Imagem do diagrama entidade relacionamento construído a partir do DBeaver.
    5. Um arquivo .sql com as consultas que mostram a quantidade de registros por tabela. Abaixo de cada consulta informe a quantidade de registros. Com comentários para definir cada tabela.

---
+++ consulta-tabelas-quantidades.sql
@@ -0,0 +1,54 @@
+-- Quantidade de registros na tabela advisor
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.advisor;
+-- Resultado: 2000
+
+-- Quantidade de registros na tabela classroom
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.classroom;
+-- Resultado: 30
+
+-- Quantidade de registros na tabela course
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.course;
+-- Resultado: 200
+
+-- Quantidade de registros na tabela department
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.department;
+-- Resultado: 20
+
+-- Quantidade de registros na tabela instructor
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.instructor;
+-- Resultado: 50
+
+-- Quantidade de registros na tabela prereq
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.prereq;
+-- Resultado: 100
+
+-- Quantidade de registros na tabela section
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.[section];
+-- Resultado: 100
+
+-- Quantidade de registros na tabela student
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.student;
+-- Resultado: 2000
+
+-- Quantidade de registros na tabela takes
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.takes;
+-- Resultado: 30000
+
+-- Quantidade de registros na tabela teaches
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.teaches;
+-- Resultado: 100
+
+-- Quantidade de registros na tabela time_slot
+SELECT COUNT(*) AS Quantidade_Registros
+FROM PTBPABD.dbo.time_slot;
+-- Resultado: 20
